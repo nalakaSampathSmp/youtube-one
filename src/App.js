c24850dd3a1e5c69c45093e71a20016d029308a8
nalakaSampathSmp <nalakasampathsmp@gmail.com>
2022-11-05 10:57:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [todoList,setTodoList]=useState([]); //todoList is variable , setTodoList is function
+  const [newTask,setNewTask]=useState(""); //newTask is variable , setNewTask is function "" is default value
+  
+  const handleChange=(event)=>{
+    setNewTask(event.target.value);
+};
+  const addTask=()=>{
+ 
+    setTodoList([...todoList,newTask])// todoList+newTask (this is variable) තියෙන list එකේ අගට 
+                                            //niwTask variable එකේ වvalue එක pass වෙනවා
+  }
 
+  const deleteTask=(taskName)=>{
+    const newTodoList=todoList.filter((task)=>{
+      if (task===taskName){
+        return false;
+
+      }else{
+        return true
+      } 
+
+    });
+    setTodoList(newTodoList);
+  }
+
+
+    return (
+
+      <div className='App'>
+        <div className='addTask'>
+        <input onChange={handleChange}/>
+        <button onClick={addTask}>Add Task</button>
+        
+        </div>
+        <div className='list'>
+          {
+            todoList.map((task)=>{
+              return <div>
+                 <h1>{task}</h1>
+                 <button onClick={()=>deleteTask(task)}>X</button>
+
+                </div>
+            })
+          }
+        </div>
+        </div>      
+    );
+}
+  
 export default App;
+
